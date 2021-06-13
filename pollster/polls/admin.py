035204943ebb858520b9961b0160a42e6f71e2a0
LazyCoder-1506 <pranayan1506@gmail.com>
2021-06-13 16:59:39 +0530

    admin area setup

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.db import models
 
-# Register your models here.
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+  fieldsets = [(None, {'fields': ['question_text']}),
+  ('Date published', {'fields': ['pub_date'], 'classes': ['collapse']}),]
+  inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
