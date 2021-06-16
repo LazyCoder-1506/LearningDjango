59e0568509995086adf9ed1b0563638262ed1503
LazyCoder-1506 <pranayan1506@gmail.com>
2021-06-16 16:54:37 +0530

    urls and templates connected, gitignore, requirements, hide secret key

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 """
 Django settings for pollster project.
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2l&xn44m$r695o4$@uz=t+o2ncf&ps&1c-p)9d^y-a2z@f79x-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'pollster.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
